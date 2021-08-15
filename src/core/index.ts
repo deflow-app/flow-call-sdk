265dccc54dc08316bb4b0288a0725f16b0243601
ALDI <aldi7th@gmail.com>
2021-08-15 22:52:06 +0800

    fix error throw bug

--- src/core/index.ts
+++ src/core/index.ts
@@ -13,8 +13,8 @@ export async function flowCall(callList: CallInfo[], variableCount: number, setV
     // const provider=new JsonRpcProvider(CHAIN_CONFIG[chainId].rpcUrl);
     logger.info("Start a flow call: ", chainId, callList, variableCount, setVariableOperationList);
     const contract = new Contract(CHAIN_CONFIG[chainId].contractAddress, abi, wallet);
+    let estiGas:BigNumber = await contract.estimateGas.flowCall(callList, variableCount, setVariableOperationList,{value:sendEthValue});
     try{
-        let estiGas:BigNumber = await contract.estimateGas.flowCall(callList, variableCount, setVariableOperationList,{value:sendEthValue});
         const tx = await contract.flowCall(callList, variableCount, setVariableOperationList,{value:sendEthValue, gasLimit:estiGas.add(estiGas.div(10))});
         const rpt = await tx.wait();
         logger.info("Flow call successful!!!");
@@ -32,8 +32,8 @@ export async function flowCallSafe(callList: CallInfo[], variableCount: number, 
     // const provider=new JsonRpcProvider(CHAIN_CONFIG[chainId].rpcUrl);
     logger.info("Start a safe flow call: ", chainId, callList, variableCount, setVariableOperationList,approvedTokens);
     const contract = new Contract(CHAIN_CONFIG[chainId].contractAddress, abi, wallet);
-    try{
-        let estiGas:BigNumber = await contract.estimateGas.flowCallSafe(callList, variableCount, setVariableOperationList,approvedTokens,{value:sendEthValue});
+    let estiGas:BigNumber = await contract.estimateGas.flowCallSafe(callList, variableCount, setVariableOperationList,approvedTokens,{value:sendEthValue});
+    try{ 
         const tx = await contract.flowCallSafe(callList, variableCount, setVariableOperationList,approvedTokens,{value:sendEthValue, gasLimit:estiGas.add(estiGas.div(10))});
         const rpt = await tx.wait();
         logger.info("Flow call successful!!!");
