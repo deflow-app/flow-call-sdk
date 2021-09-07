bd3dcf7c0e0995fe7d2231a05cc1874dc809ee8d
ALDI <aldi7th@gmail.com>
2021-09-07 10:33:38 +0800

    unbind aggregatorsdk

--- src/entities/pageModel.ts
+++ src/entities/pageModel.ts
@@ -1,116 +1,119 @@
-import { Token } from "aggregator-sdk";
-import {CallFuncParamType, VariableType, TokenApprovalResState, PubType, ChainId, Source} from "../core";
-import { TriggerType, ExternalCallInfo, CallType} from "./contractModel";
+import { CallFuncParamType, VariableType, TokenApprovalResState, PubType, ChainId, Source } from "../core";
+import { TriggerType, ExternalCallInfo, CallType } from "./contractModel";
 import { BigNumberish } from "ethers";
 
 
-export interface PubObj{
-    type:PubType,
-    data:SuperContract|CronJob
+export interface Token {
+    chainId: ChainId, address: string, decimals: number, symbol?: string, name?: string, logoUrl?: string
 }
 
-export interface CronJob{
-    key:string,
-    name:string,
-    cron:string,
-    scheduler:TaskRunnerConf[],
-    chainId:ChainId,
-    latestCID?:string,
-    src:Source,
-    runners?:TaskRunner[],
-    tasks?:{key:string, task:SuperContract}[]
+export interface PubObj {
+    type: PubType,
+    data: SuperContract | CronJob
 }
 
-export interface TaskRunnerConf{
-    taskRunnerKeys:{key:string, seq:number}[], 
-    seq:number,
-    isParalelle:boolean, 
+export interface CronJob {
+    key: string,
+    name: string,
+    cron: string,
+    scheduler: TaskRunnerConf[],
+    chainId: ChainId,
+    latestCID?: string,
+    src: Source,
+    runners?: TaskRunner[],
+    tasks?: { key: string, task: SuperContract }[]
+}
+
+export interface TaskRunnerConf {
+    taskRunnerKeys: { key: string, seq: number }[],
+    seq: number,
+    isParalelle: boolean,
     exitOnError: boolean
 }
 
-export enum InputWhenRunType{
-    call=0,
-    setVariableOperation=1
+export enum InputWhenRunType {
+    call = 0,
+    setVariableOperation = 1
 }
 
-export interface InputWhenRun{
-    inputWhenRunType:InputWhenRunType,
-    callId:string,
-    callFuncParamName:string,
-    setVariableOperationId:string,
-    type:string,
-    value:any
+export interface InputWhenRun {
+    inputWhenRunType: InputWhenRunType,
+    callId: string,
+    callFuncParamName: string,
+    setVariableOperationId: string,
+    type: string,
+    value: any
 }
 
-export interface TaskRunner{
-    key:string,
-    name:string,
-    contractKey:string,
+export interface TaskRunner {
+    key: string,
+    name: string,
+    contractKey: string,
     // externalVars:{[key:string]:any}[],
-    inputsWhenRun?:InputWhenRun[],
-    safeMode?:boolean
+    inputsWhenRun?: InputWhenRun[],
+    safeMode?: boolean
 }
 
-export interface SuperContract{
-    calls:SuperCall[];
-    variables:SuperContractVariable[];
-    setVariableOperations:SetVariableOperation[];
-    chainId:ChainId;
-    approveTokens?:Token[];
-    latestCID?:string;
+export interface SuperContract {
+    calls: SuperCall[];
+    variables: SuperContractVariable[];
+    setVariableOperations: SetVariableOperation[];
+    chainId: ChainId;
+    approveTokens?: Token[];
+    latestCID?: string;
 }
 
-export enum SubType{
-    normal=0,
-    sendETH=1
+export enum SubType {
+    normal = 0,
+    sendETH = 1
 }
 
-export interface SuperCall{
-    callType:CallType;
-    subType?:SubType;
-    id:string;
-    name:string;
-    seq:number;
-    callCondition?:string;
-    contractAddr?:string;
-    contractName?:string;
-    abiInfo?:any;
-    callFunc?:CallFunc;
-    sendEthValue?:BigNumberish;
-    tokenAmount?:BigNumberish|string;
-    tokenDecimals?:number;
-    tokenSymbol?:string;
+export interface SuperCall {
+    callType: CallType;
+    subType?: SubType;
+    id: string;
+    name: string;
+    seq: number;
+    callCondition?: string;
+    contractAddr?: string;
+    contractName?: string;
+    abiInfo?: any;
+    callFunc?: CallFunc;
+    sendEthValue?: BigNumberish;
+    tokenAmount?: BigNumberish | string;
+    tokenDecimals?: number;
+    tokenSymbol?: string;
 }
 
-export interface CallFunc{
-    name:string;
-    params:CallFuncParam[];
-    returnValuesCount:number;
+export interface CallFunc {
+    name: string;
+    params: CallFuncParam[];
+    returnValuesCount: number;
 }
 
-export interface CallFuncParam{
-    name:string;
-    type:CallFuncParamType;
-    value:any;
-    contractName?:string;
-    tokenSymbol?:string;
+export interface CallFuncParam {
+    name: string;
+    type: CallFuncParamType;
+    value: any;
+    contractName?: string;
+    tokenSymbol?: string;
 }
 
-export interface SetVariableOperation{
-    id:string;
-    triggerId:string;
-    type:TriggerType;
-    valueExp:string;
-    variableCodeToSet:string;
+export interface SetVariableOperation {
+    id: string;
+    triggerId: string;
+    type: TriggerType;
+    valueExp: string;
+    variableCodeToSet: string;
 }
 
-export interface SuperContractVariable{
-    code:string,
-    name:string,
-    type:VariableType
+export interface SuperContractVariable {
+    code: string,
+    name: string,
+    type: VariableType
 }
 
-export interface TransactionReceiptEvent{
+export interface TransactionReceiptEvent {
     address: string,
     args: Array<any>,
     blockHash: string,
@@ -124,35 +127,35 @@ export interface TransactionReceiptEvent{
     transactionIndex: number
 }
 
-export interface TransactionEventInfo{
-    event:string,
-    eventSignature:string,
-    args:Array<any>,
-    externalCall?:ExternalCallInfo
+export interface TransactionEventInfo {
+    event: string,
+    eventSignature: string,
+    args: Array<any>,
+    externalCall?: ExternalCallInfo
 }
 
-export interface TokenApproval{
-    chainId:ChainId,
-    approvalList?:Array<TokenApprovalItem>
+export interface TokenApproval {
+    chainId: ChainId,
+    approvalList?: Array<TokenApprovalItem>
 }
 
-export interface TaskExecuteResult{
-    isSuccess:boolean,
-    task?:SuperContract,
-    runner?:TaskRunner,
-    errMsg?:string,
-    reciept?:any,
-    events?:TransactionEventInfo[]
+export interface TaskExecuteResult {
+    isSuccess: boolean,
+    task?: SuperContract,
+    runner?: TaskRunner,
+    errMsg?: string,
+    reciept?: any,
+    events?: TransactionEventInfo[]
 }
 
 export type TokenApprovalItem = {
-    chainId:ChainId,
-    tokenAddr:string,
-    amount:BigNumberish
+    chainId: ChainId,
+    tokenAddr: string,
+    amount: BigNumberish
 }
 
 export type TokenApprovalRes = {
-    tokenAddr:string,
-    state:TokenApprovalResState,
-    tokenDecimal:number
+    tokenAddr: string,
+    state: TokenApprovalResState,
+    tokenDecimal: number
 }
